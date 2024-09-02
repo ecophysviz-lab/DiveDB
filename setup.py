f6357f5c7bd9422d4523e496c02560c11f9d199f
Will Gislason <8203830+gislawill@users.noreply.github.com>
2024-09-02 15:47:29 -0700

    exports

--- setup.py
+++ setup.py
@@ -3,6 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="DiveDB",
     version="0.1.0",
-    packages=find_packages(include=["services", "services.*", "server", "server.*"]),
+    packages=find_packages(
+        include=[
+            "services",
+            "services.*",
+            "services.utils",
+            "services.utils.*",
+            "server",
+            "server.*",
+            "server.django_app",
+            "server.django_app.*",
+            "server.metadata",
+            "server.metadata.*",
+        ]
+    ),
     include_package_data=True,
 )
