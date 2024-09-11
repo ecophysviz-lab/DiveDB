fbdfc42e9860b503482f4b58814b8952d9273b22
Will Gislason <8203830+gislawill@users.noreply.github.com>
2024-09-11 15:43:30 -0700

    ci test updates

--- conftest.py
+++ conftest.py
@@ -1,17 +1,15 @@
 from unittest.mock import patch
 
-# Apply the patch at the module level
-patcher = patch("DiveDB.services.utils.storage.SwiftClient")
-MockSwiftClient = patcher.start()
-instance = MockSwiftClient.return_value
-instance.get_containers.return_value = []
-instance.list_objects.return_value = []
-instance.get_object_binary.return_value = b""
-instance.write_object_to_local.return_value = None
-instance.create_container.return_value = None
-instance.put_object.return_value = "mocked_url"
 
-
-def pytest_configure(config):
+def pytest_load_initial_conftests(early_config, parser, args):
     print("Mocking SwiftClient")
-    config.add_cleanup(patcher.stop)
+    patcher = patch("DiveDB.services.utils.storage.SwiftClient")
+    MockSwiftClient = patcher.start()
+    instance = MockSwiftClient.return_value
+    instance.get_containers.return_value = []
+    instance.list_objects.return_value = []
+    instance.get_object_binary.return_value = b""
+    instance.write_object_to_local.return_value = None
+    instance.create_container.return_value = None
+    instance.put_object.return_value = "mocked_url"
+    early_config.add_cleanup(patcher.stop)
