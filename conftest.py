2c297a192e9d15fa60cee36c0a837c80cf09591c
Will Gislason <8203830+gislawill@users.noreply.github.com>
2024-09-10 22:07:44 -0700

    ci test updates

--- conftest.py
+++ conftest.py
@@ -2,14 +2,18 @@ import pytest
 from unittest.mock import patch
 
 
-@pytest.fixture(scope="session", autouse=True)
-def mock_swiftclient():
-    with patch("DiveDB.services.utils.storage.SwiftClient") as MockSwiftClient:
-        instance = MockSwiftClient.return_value
-        instance.get_containers.return_value = []
-        instance.list_objects.return_value = []
-        instance.get_object_binary.return_value = b""
-        instance.write_object_to_local.return_value = None
-        instance.create_container.return_value = None
-        instance.put_object.return_value = "mocked_url"
-        yield instance
+def pytest_configure():
+    patcher = patch("DiveDB.services.utils.storage.SwiftClient")
+    MockSwiftClient = patcher.start()
+    instance = MockSwiftClient.return_value
+    instance.get_containers.return_value = []
+    instance.list_objects.return_value = []
+    instance.get_object_binary.return_value = b""
+    instance.write_object_to_local.return_value = None
+    instance.create_container.return_value = None
+    instance.put_object.return_value = "mocked_url"
+    pytest.mock_patcher = patcher
+
+
+def pytest_unconfigure():
+    pytest.mock_patcher.stop()
