aa16281f241860bd7835913dee0963ece0ee413f
Christopher J Lowrie <chlowrie@Christophers-MacBook-Pro.local>
2024-09-16 11:23:51 -0400

    Adding Jupyter

--- deployment/jupyter/build.sh
+++ deployment/jupyter/build.sh
@@ -4,10 +4,10 @@ source .env
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Script directory: $SCRIPT_DIR"
 
-kubectl delete -f $SCRIPT_DIR/django.deployment.yaml
-kubectl apply -f $SCRIPT_DIR/django.deployment.yaml
-kubectl apply -f $SCRIPT_DIR/django.service.yaml
-kubectl apply -f $SCRIPT_DIR/django.ingress.yaml
+kubectl delete -f $SCRIPT_DIR/jupyter.deployment.yaml
+kubectl apply -f $SCRIPT_DIR/jupyter.deployment.yaml
+kubectl apply -f $SCRIPT_DIR/jupyter.service.yaml
+kubectl apply -f $SCRIPT_DIR/jupyter.ingress.yaml
 
 
 
