5ab271788ada74348eb6de7bafe2c7c5bed444d5
Christopher J Lowrie <chlowrie@Christophers-MacBook-Pro.local>
2024-09-17 10:29:50 -0400

    Updates to exec and migrate

--- deployment/exec.sh
+++ deployment/exec.sh
@@ -1,1 +1,3 @@
-# kubectl exec -it divedb-django-deployment-796c9d764c-477bd -- bash
+POD_NAME=$(kubectl get pods -l app=divedb-django -o jsonpath='{.items[0].metadata.name}')
+
+kubectl exec -it $POD_NAME -- bash
