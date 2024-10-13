12d536895e1f1f33c55089e2ea3bbe46aac820ae
Cade Mirchandani <cmirchan@ucsc.edu>
2024-10-13 13:21:59 -0700

    feat: validate netcdf (#27)
    * initial commit for validate netcdf
    
    * build fixes
    
    * build fixes - postgres connection
    
    * build fixes - postgres connection
    
    ---------
    
    Co-authored-by: Will Gislason <8203830+gislawill@users.noreply.github.com>

---
+++ tests/services/test_data_uploader.py
@@ -0,0 +1,47 @@
+import pytest
+import xarray as xr
+import numpy as np
+import pandas as pd
+from DiveDB.services.data_uploader import DataUploader
+from DiveDB.services.data_uploader import NetCDFValidationError
+
+
+@pytest.fixture
+def valid_netcdf_dataset():
+    time_samples = pd.date_range("2023-01-01", periods=5, freq="D")
+    variable_labels = ["label1", "label2"]
+
+    ds = xr.Dataset(
+        {
+            "data_var1": (("sensor_samples",), np.random.rand(5)),
+            "data_var2": (
+                ("sensor_samples", "sensor_variables"),
+                np.random.rand(5, 2),
+            ),
+        },
+        coords={
+            "sensor_samples": ("sensor_samples", time_samples),
+            "sensor_variables": ("sensor_variables", variable_labels),
+        },
+    )
+
+    # Set the necessary attributes
+    ds["data_var1"].attrs["variable"] = "example_variable"
+    ds["data_var2"].attrs["variables"] = ["label1", "label2"]
+
+    return ds
+
+
+def test_validate_data_valid(valid_netcdf_dataset):
+    uploader = DataUploader()
+    ds = valid_netcdf_dataset
+    assert uploader._validate_netcdf(ds)
+
+
+def test_validate_data_invalid(valid_netcdf_dataset):
+    uploader = DataUploader()
+    ds = valid_netcdf_dataset
+    ds = ds.rename({"sensor_samples": "sensor"})
+
+    with pytest.raises(NetCDFValidationError):
+        uploader._validate_netcdf(ds)
