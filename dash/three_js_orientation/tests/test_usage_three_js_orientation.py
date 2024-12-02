eb49d67875cb7c330998f411207a5a789b072eac
Will Gislason <8203830+gislawill@users.noreply.github.com>
2024-12-02 11:16:40 -0600

    Testing updates

--- dash/three_js_orientation/tests/test_usage_three_js_orientation.py
+++ dash/three_js_orientation/tests/test_usage_three_js_orientation.py
@@ -3,6 +3,10 @@ from dash.testing.application_runners import import_app
 
 # Basic test for the component rendering.
 # The dash_duo pytest fixture is installed with dash (v1.0+)
+
+
+# TODO: Add tests for this custom dash components
+# This test is currently failing
 def test_render_component(dash_duo):
     # Start a dash app contained as the variable `app` in `usage.py`
     app = import_app("usage")
