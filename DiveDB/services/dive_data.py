4e14d961d118fb0693b9c9e8923e88845d865286
hannahilea <hannahilea@users.noreply.github.com>
2025-01-29 17:46:00 -0500

    refactor to pull out helper functions

--- DiveDB/services/dive_data.py
+++ DiveDB/services/dive_data.py
@@ -75,8 +75,7 @@ def export_to_edf(data: DiveData, output_dir: str) -> list:
     edf_filepaths = []
     data.get_metadata()  # TODO-CHECK: always, or only if it's None already?
     for recording_id in data.metadata.keys():
-        df = data.duckdb_relation.filter("recording='{recording_id}'").df()
-        print(df)
+        df = data.duckdb_relation.filter(f"recording='{recording_id}'").df()
         recording_metadata = data.metadata[recording_id]
         edf = construct_recording_edf(df, recording_metadata)
         edf_path = make_unique_edf_filename(output_dir, recording_id)
@@ -85,30 +84,94 @@ def export_to_edf(data: DiveData, output_dir: str) -> list:
     return edf_filepaths
 
 
+# For now, assumes timestamps are contiguous and uniformly sampled
+# Due to floating point differences when sampling rate was originally stored in 
+# timestamp format, 
+# TODO-file-future: If there are gaps in the timestamps, throw a warning (and/or support them)
+# TODO-file-future: Check that sampling rate is uniform and isn't missing samples
+def get_sampling_rate(timestamps):
+    print("HERE", type(timestamps))
+    sample_diffs_sec = timestamps.flatten().diff[1:].dt.total_seconds()
+    sampling_interval_mean = sample_diffs_sec.mode()
+    return 1 / sampling_interval_mean
+
+
+# TODO-file-future: Should be on a per-signal basis, either hard-coded (eh) or passed in
+# by the user (better) or gleaned from the db (best, but requires storing in db!)
+def get_pad_value_for_signal(signal_name):
+    print("Warning: signal-specific padding value not yet implemented; defaulting to `0`")
+    return np.float64(0)
+
+
+# Correctness note: The start time relative to the start of the recording (and therefore
+# other signals) may be off by as much as 1/sampling_rate seconds. ¯\_(ツ)_/¯
+# Nothing we can do---such is the life of conforming to the EDF spec!
+def get_signal_offset_index(signal_starttime: datetime, sampling_rate,
+                            recording_starttime):
+    # We know (by definition of how the recording start time was constructed) that
+    # the difference here will always be positive
+    offset_sec = (recording_starttime - signal_starttime).total_seconds()
+    return round(offset_sec * sampling_rate)
+
+
+# Conform to EDF spec naming requirements
+# TODO-file-future: Define specific rules for constructing signal name from class and label
+# TODO-file-future: Validate that signal names are unique within file
+def signal_name(signal_class_name, signal_name):
+    if signal_class_name.startswith("sensor_data"):
+        class_prefix = signal_class_name[12:]
+    elif signal_class_name.startswith("derived_data"):
+        class_prefix = "**" + signal_class_name[13:]
+    else:
+        class_prefix = signal_class_name
+
+    if signal_class_name == signal_name:
+        label = class_prefix if len(class_prefix) <= 16 else class_prefix[0:16]  # lol EDF
+    else:
+        max_prefix_length = 16 - len(signal_name) - 1  # lol EDF 
+        # TODO handle case when prefix is now < 0
+        class_prefix = class_prefix if len(class_prefix) <= max_prefix_length else class_prefix[0:max_prefix_length]
+        label = class_prefix + "-" + signal_name
+    return label
+
+
+# TODO-file-future: Handle differently (or don't return at all! might be feature creep)
+def data_processing_details(signal_class_name):
+    if signal_class_name.startswith("derived_data"):
+         return "derived"
+    elif signal_class_name.startswith("sensor_data"):
+         return "sensor"
+    else:
+         return ""
+
+
+# TODO-file-future: Should be on a per-signal basis, either hard-coded (eh) or passed in
+# by the user (better) or gleaned from the db (best, but requires storing in db!)
+def get_physical_dimension(signal_name):
+    return "Unknown"
+
+
+# TODO: implement (or don't return at all! might be feature creep?)
+def sensor_type_details(name, metadata):
+     return "TODO"  # TODO - question: are there length limits here, in the EDF spec??
+
+
+# Assumes we've been passed a single recording and that all signals in it are valid and continuous
+# TODO-future-file: validate that individual signals are not discontinuous
 def construct_recording_edf(df, metadata):
     # Iterate through each signal (class + label) in the recording 
     signal_names = [n for n in list(df.columns)if n not in ['datetime', 'class', 'recording']]
 
-    # Set up: Figure out what common max duration is
-    max_duration_sec = 0
-    start_datetime = None
-    for name in signal_names:
-        df_sig = df[['datetime', name]]
-        df_sig = df_sig[df_sig[name].notna()]
-        df_sig = df_sig.sort_values(by=['datetime'])
+    # EDF signals must all be the same length within an EDF record
+    # (in seconds, irrespective of sampling rate)---so figure out what those bounds 
+    # are before doing anything else
+    all_timestamps = df.sort_values('datetime')['datetime'].values
+    recording_start_datetime = Timestamp(all_timestamps[0]).to_pydatetime()
+    recording_end_datetime = Timestamp(all_timestamps[-1]).to_pydatetime()
+    print(recording_start_datetime, recording_end_datetime, signal_names)
 
-        sampling_rate = df_sig['datetime'].diff()[1:].dt.total_seconds().unique()[0] #TODO-safety: handle correctly if there are any gaps filtered out (eg from nonna() earlier)
-        sampling_frequency = int(1/sampling_rate) # TODO-safety: don't just blindly round o_O
-
-        sig_start_datetime = df_sig['datetime'].values[0]
-        if start_datetime is not None:
-            if sig_start_datetime != start_datetime:
-                print("Warning: Signals start at different times! Not yet handled by \
-                      EDF export! ", start_datetime, sig_start_datetime)
-        start_datetime = sig_start_datetime
-
-        sig_max_duration_sec = math.ceil(df.shape[0] / sampling_frequency)
-        max_duration_sec = max(max_duration_sec, sig_max_duration_sec)
+    # For EDF reasons, round the total duration up so to the nearest second
+    recording_duration_sec = math.ceil((recording_end_datetime - recording_start_datetime).total_seconds())
 
     signals = []
     for name in signal_names:
@@ -116,65 +179,37 @@ def construct_recording_edf(df, metadata):
         df_sig = df_sig[df_sig[name].notna()]
         df_sig = df_sig.sort_values(by=['datetime'])
 
-        # TODO-safety: check that there's only one value after the unique, check that 
-        # this is an integer value or whatever the EDF spec requires, etc
-        sampling_rate = df_sig["datetime"].diff()[1:].dt.total_seconds().unique()[0]
-        sampling_frequency = int(1/sampling_rate) # TODO-safety: don't just blindly round, see if we allow floating point values here also?? o_O
-        # Safety skipping: make sure no gaps, make sure even sample spacing, set start time relative to overall recording
+        # Construct the signal data, padded relative to the overall recording
+        timestamps = df_sig.sort_values('datetime')['datetime'].values
+        sampling_rate = get_sampling_rate(timestamps)
+        i_start_sample = get_signal_offset_index(timestamps[0],
+                                                sampling_rate, recording_start_datetime)
+        pad_value = get_pad_value_for_signal(name)
+        signal_data = np.full(recording_duration_sec * sampling_rate, pad_value,
+                              dtype=np.float64)
+        signal_data[i_start_sample:timestamps.len()] = df_sig[name].values
 
-        # Need to figure out max signal length, then start time, then 
-        # Lpad to the correct start time + lpad to the correct stop time (lol EDF)
-        # TODO-future: instead of padding w/ 0, use some signal-specific value
-        signal_data = np.zeros(max_duration_sec * sampling_frequency, dtype=np.float64)
-        num_samples = len(df_sig[name].values)
-        i_sample_start_offset = 0 #TODO - make sure this is set to the signal's offset
-        signal_data[i_sample_start_offset:num_samples] = df_sig[name].values
-
-        class_name = df_sig['class'].unique()[0]
-        if class_name.startswith("sensor_data"):
-            class_prefix = class_name[12:]
-        elif class_name.startswith("derived_data"):
-            class_prefix = "**" + class_name[13:]
-        else:
-            class_prefix = class_name
-
-        # TODO-future safety: make sure signal labels are unique across recording/edf
-        # TODO-future: pull into own function
-        if class_name == name:
-            label = class_prefix if len(class_prefix) <= 16 else class_prefix[0:16]  # lol EDF
-        else:
-            max_prefix_length = 16 - len(name) - 1  # lol EDF 
-            # TODO handle case when prefix is now < 0
-            class_prefix = class_prefix if len(class_prefix) <= max_prefix_length else class_prefix[0:max_prefix_length]
-            label = class_prefix + "-" + name
-
+        # Assemble the signal metadata
+        signal_class = df_sig['class'].unique()[0]
+        label = signal_name(signal_class, name)
+        physical_dimension = get_physical_dimension(name)
+        processing_details_str = data_processing_details(signal_class)
+        sensor_type_str = sensor_type_details(name, metadata)
+        
         signal = EdfSignal(signal_data,
-                            sampling_frequency=sampling_frequency, 
-                            physical_dimension="TODO",
-                            label=label
-                            # transducer_type="AgAgCl electrode",
-                            # prefiltering="HP:0.1Hz LP:75Hz"
-                            )
-        # TODO-add header metadata
+                           sampling_frequency=sampling_rate,
+                           label=label,
+                           physical_dimension=physical_dimension,
+                           transducer_type=sensor_type_str,
+                           prefiltering=processing_details_str)
+        # TODO-any additional metadata to include?? check spec.
         signals.append(signal)
 
-    starttime = Timestamp(start_datetime).to_pydatetime().time()
     edf = Edf(signals,
-              starttime=starttime)
-    
-    # Add recording header
-    startdate = Timestamp(start_datetime).to_pydatetime().date()
-    edf.recording = Recording(
-        startdate=startdate,
-        # hospital_administration_code="EMG561",
-        # investigator_technician_code="BK/JOP",
-        # equipment_code="Sony",
-    )
-
-    edf.patient=Patient(
-        # code="MCH-0234567",
-        # sex="F",
-        # birthdate=datetime.date(1951, 5, 2),
-        # name="Haagse_Harry",
+              starttime=recording_start_datetime.time(),
+              startdate=recording_start_datetime.date())
+    # edf.recording = Recording(startdate=startdate)
+    edf.patient = Patient(
+        code="MCH-0234567", #TODO - metadata animal
     )
     return edf
