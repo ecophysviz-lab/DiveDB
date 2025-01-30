ecad81f1aafa1ad9c3623d927ee4dc53f9ff6c7d
hannahilea <hannahilea@users.noreply.github.com>
2025-01-29 20:07:09 -0500

    clean up refactor, now functional

--- DiveDB/services/dive_data.py
+++ DiveDB/services/dive_data.py
@@ -6,20 +6,23 @@ import duckdb
 import math
 import numpy as np
 import datetime
-from pandas import Timestamp
-from edfio import Edf, EdfSignal, Recording, Patient
+from pandas import Timestamp, Series
+from edfio import Edf, EdfSignal, Patient, Recording
 import os.path
+from pathlib import Path
 
 
-# TODO-clarify: spec says DuckDBPyConnection but get_delta_data returns a Relation 
-# (and explicitly says it doesn't pull data into memory)---using a relation here 
-# Followed proxy pattern to extend class: https://stackoverflow.com/questions/69313845/create-child-class-object-using-parent-class-instance
+# TODO-clarify: spec says DuckDBPyConnection but get_delta_data returns a Relation
+# (and explicitly says it doesn't pull data into memory)---using a relation here
+# Followed proxy pattern to extend class: 
+# https://stackoverflow.com/questions/69313845/create-child-class-object-using-parent-class-instance
 class DiveData():
     """EDF Export Manager"""
     def __init__(self, duckdb_relation: duckdb.DuckDBPyRelation, conn: duckdb.DuckDBPyConnection):
         self.duckdb_relation = duckdb_relation
         self.recording_ids = [id for id in duckdb_relation.unique('recording').df()['recording'].values]
-        # TODO-check: based on spec, required to set this here... Seems better to calculate metadata immediately and not save, though...
+        # TODO-check: based on spec, required to set this here... Seems better to
+        # calculate metadata immediately and not save, though...
         self.conn = conn
         self.metadata = None
 
@@ -37,7 +40,7 @@ class DiveData():
         return export_to_edf(self, output_dir)
 
 #####
-##### Utils
+# Utils
 #####
 
 
@@ -52,7 +55,7 @@ def get_metadata(divedata: DiveData):
                             WHERE Recordings.id = '{recording_id}'
                         """).df()
         if df.shape[0] != 1:
-            print("Oh no more than one item for this recording") # TODO-throw error 
+            print("Oh no more than one item for this recording")  # TODO-throw error 
         for col in list(['start_time', 'animal_deployment_id', 'logger_id']):
             recording_metadata[col] = df[col][0]
         metadata[recording_id] = recording_metadata
@@ -73,6 +76,7 @@ def make_unique_edf_filename(output_dir, filename_prefix: str, max_suffix_int: i
 def export_to_edf(data: DiveData, output_dir: str) -> list:
     """Export metadata plus signals to set of EDF files"""
     edf_filepaths = []
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
     data.get_metadata()  # TODO-CHECK: always, or only if it's None already?
     for recording_id in data.metadata.keys():
         df = data.duckdb_relation.filter(f"recording='{recording_id}'").df()
@@ -85,21 +89,21 @@ def export_to_edf(data: DiveData, output_dir: str) -> list:
 
 
 # For now, assumes timestamps are contiguous and uniformly sampled
-# Due to floating point differences when sampling rate was originally stored in 
-# timestamp format, 
-# TODO-file-future: If there are gaps in the timestamps, throw a warning (and/or support them)
+# Due to floating point differences when sampling rate was originally stored in
+# timestamp format,
+# TODO-file-future: If there are gaps in the timestamps, throw a warning (and/or
+# support them)
 # TODO-file-future: Check that sampling rate is uniform and isn't missing samples
-def get_sampling_rate(timestamps):
-    print("HERE", type(timestamps))
-    sample_diffs_sec = timestamps.flatten().diff[1:].dt.total_seconds()
-    sampling_interval_mean = sample_diffs_sec.mode()
-    return 1 / sampling_interval_mean
+def get_sampling_rate(timestamps: Series):
+    sample_diffs_sec = timestamps.diff()[1:].dt.total_seconds()
+    sampling_interval_median = sample_diffs_sec.median()
+    return 1 / sampling_interval_median
 
 
 # TODO-file-future: Should be on a per-signal basis, either hard-coded (eh) or passed in
 # by the user (better) or gleaned from the db (best, but requires storing in db!)
 def get_pad_value_for_signal(signal_name):
-    print("Warning: signal-specific padding value not yet implemented; defaulting to `0`")
+    print(f"Warning: Signal-specific padding value not yet implemented for `{signal_name}` (default: `0`")
     return np.float64(0)
 
 
@@ -107,11 +111,12 @@ def get_pad_value_for_signal(signal_name):
 # other signals) may be off by as much as 1/sampling_rate seconds. ¯\_(ツ)_/¯
 # Nothing we can do---such is the life of conforming to the EDF spec!
 def get_signal_offset_index(signal_starttime: datetime, sampling_rate,
-                            recording_starttime):
+                            recording_starttime: datetime):
     # We know (by definition of how the recording start time was constructed) that
     # the difference here will always be positive
     offset_sec = (recording_starttime - signal_starttime).total_seconds()
-    return round(offset_sec * sampling_rate)
+    i = round(offset_sec * sampling_rate)
+    return i
 
 
 # Conform to EDF spec naming requirements
@@ -138,11 +143,11 @@ def signal_name(signal_class_name, signal_name):
 # TODO-file-future: Handle differently (or don't return at all! might be feature creep)
 def data_processing_details(signal_class_name):
     if signal_class_name.startswith("derived_data"):
-         return "derived"
+        return "derived"
     elif signal_class_name.startswith("sensor_data"):
-         return "sensor"
+        return "sensor"
     else:
-         return ""
+        return ""
 
 
 # TODO-file-future: Should be on a per-signal basis, either hard-coded (eh) or passed in
@@ -153,7 +158,7 @@ def get_physical_dimension(signal_name):
 
 # TODO: implement (or don't return at all! might be feature creep?)
 def sensor_type_details(name, metadata):
-     return "TODO"  # TODO - question: are there length limits here, in the EDF spec??
+    return "TODO"  # TODO - question: are there length limits here, in the EDF spec??
 
 
 # Assumes we've been passed a single recording and that all signals in it are valid and continuous
@@ -165,10 +170,9 @@ def construct_recording_edf(df, metadata):
     # EDF signals must all be the same length within an EDF record
     # (in seconds, irrespective of sampling rate)---so figure out what those bounds 
     # are before doing anything else
-    all_timestamps = df.sort_values('datetime')['datetime'].values
-    recording_start_datetime = Timestamp(all_timestamps[0]).to_pydatetime()
-    recording_end_datetime = Timestamp(all_timestamps[-1]).to_pydatetime()
-    print(recording_start_datetime, recording_end_datetime, signal_names)
+    all_timestamps = df.sort_values('datetime')['datetime']
+    recording_start_datetime = Timestamp(all_timestamps.iloc[0]).to_pydatetime()
+    recording_end_datetime = Timestamp(all_timestamps.iloc[-1]).to_pydatetime()
 
     # For EDF reasons, round the total duration up so to the nearest second
     recording_duration_sec = math.ceil((recording_end_datetime - recording_start_datetime).total_seconds())
@@ -180,14 +184,15 @@ def construct_recording_edf(df, metadata):
         df_sig = df_sig.sort_values(by=['datetime'])
 
         # Construct the signal data, padded relative to the overall recording
-        timestamps = df_sig.sort_values('datetime')['datetime'].values
+        timestamps = df_sig.sort_values('datetime')['datetime']
         sampling_rate = get_sampling_rate(timestamps)
-        i_start_sample = get_signal_offset_index(timestamps[0],
-                                                sampling_rate, recording_start_datetime)
+        signal_start_datetime = Timestamp(timestamps.iloc[0]).to_pydatetime()
+        i_start_sample = get_signal_offset_index(signal_start_datetime,
+                                                 sampling_rate, recording_start_datetime)
         pad_value = get_pad_value_for_signal(name)
-        signal_data = np.full(recording_duration_sec * sampling_rate, pad_value,
+        signal_data = np.full(math.ceil(recording_duration_sec * sampling_rate), pad_value,
                               dtype=np.float64)
-        signal_data[i_start_sample:timestamps.len()] = df_sig[name].values
+        signal_data[i_start_sample:timestamps.size] = df_sig[name].values
 
         # Assemble the signal metadata
         signal_class = df_sig['class'].unique()[0]
@@ -206,10 +211,9 @@ def construct_recording_edf(df, metadata):
         signals.append(signal)
 
     edf = Edf(signals,
-              starttime=recording_start_datetime.time(),
-              startdate=recording_start_datetime.date())
-    # edf.recording = Recording(startdate=startdate)
+              starttime=recording_start_datetime.time())
+    edf.recording = Recording(startdate=recording_start_datetime.date())
     edf.patient = Patient(
-        code="MCH-0234567", #TODO - metadata animal
+        code="MCH-0234567",  # TODO - metadata animal
     )
     return edf
